1deb62cf0860c2b31a9d1dd6af52df8a28a8cea9
Cale Fitzwater <calefitzwater@gmail.com>
2021-07-24 13:16:37 -0400

    Health

--- Assets/Scripts/PlayerHealth.cs
+++ Assets/Scripts/PlayerHealth.cs
@@ -1,10 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerHealth : MonoBehaviour
 {
     public int Health = 100;
+    public Image healthBar;
 
     private float timeDelay = 0;
     void Update()
@@ -13,17 +15,20 @@ public class PlayerHealth : MonoBehaviour
         {
             timeDelay -= Time.deltaTime;
         }
-        else
+        if(timeDelay < 0)
         {
+            print("RAN");
             timeDelay = 0;
         }
+        print(timeDelay);
+        healthBar.fillAmount = Mathf.Clamp(Health / 100f, 0, 1f);
     }
     private void OnTriggerStay2D(Collider2D collision)
     {
-        if(collision.gameObject.tag == "Fire" && timeDelay <= 0)
+        if(collision.gameObject.tag == "Fire" && timeDelay <= .01f)
         {
             timeDelay = 1;
-            Health -= 5;
+            Health -= 15;
         }
     }
 }
